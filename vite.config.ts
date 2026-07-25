da4646fd9633f6d1c030f5c5943b63a5178f2660
NIHAR KUMAR PATEL <niharkumarpatel01@gmail.com>
2026-07-26 02:05:14 +0530

    Update Website

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,14 @@
 import { defineConfig } from 'vite'
 import { tanstackStart } from '@tanstack/react-start/plugin/vite'
+import tailwindcss from '@tailwindcss/vite'
 import netlify from '@netlify/vite-plugin-tanstack-start'
+import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
   plugins: [
+    tsconfigPaths(),
+    tailwindcss(),
     tanstackStart(),
     netlify(),
-    // your other existing plugins
   ],
 })
